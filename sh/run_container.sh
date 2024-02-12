676113a2436dd9ba1eb2252c50e752e55758ff6c
squidmin <morse.james.r@gmail.com>
2024-02-12 09:49:41 -0600

    Typo

--- sh/run_container.sh
+++ sh/run_container.sh
@@ -7,4 +7,4 @@ docker run \
   -e GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS} \
   -v $HOME/.config/gcloud:/root/.config/gcloud \
   -v $HOME/.m2:/root/.m2 \
-  java11-spring-gradle-bigquery-reference
+  java17-spring-gradle-bigquery-reference
